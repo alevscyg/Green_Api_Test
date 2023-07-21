cb7e93f258ff3dc94d115fe328c3747cd0785be2
alevscyg <110639375+alevscyg@users.noreply.github.com>
2023-07-21 20:38:08 +0500

    Update m2.service.ts

--- apps/m2/src/m2.service.ts
+++ apps/m2/src/m2.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class M2Service {
-  getHello(): string {
-    return 'Green_Api';
+  async getHello() {
+    return await 'Green_Api';
   }
 }
